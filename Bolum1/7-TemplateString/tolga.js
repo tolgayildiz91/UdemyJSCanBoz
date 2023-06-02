48b7778534dcaf63c3cab6e767ae370054f20357
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 19:00:55 +0300

    template string

---
+++ Bolum1/7-TemplateString/tolga.js
@@ -0,0 +1,18 @@
+const spor1="Yamaç Paraşütü";
+const spor2="Tüplü Dalış";
+const spor3="Snowboard";
+
+let sonuc='Tolga Yıldız ın yaptığı sporlar: '+spor1+ ' ' + spor2 + ' ' + spor3
+console.log(sonuc)
+
+let sonuc1 =`Tolga Yıldız ın yaptığı sporlar: ${spor1} ${spor2} ${spor3} `
+console.log(sonuc1)
+
+let html=`
+<h1>${spor1} </h1>
+<h2>${spor2} </h2>
+<h3>${spor3} </h3>
+
+`
+
+console.log(html)
