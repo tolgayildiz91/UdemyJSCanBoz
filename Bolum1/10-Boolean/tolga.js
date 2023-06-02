483b9f35281a6f2d95d3101b42b3d3867f617bc6
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 19:27:57 +0300

    boolean

---
+++ Bolum1/10-Boolean/tolga.js
@@ -0,0 +1,28 @@
+//console.log(true,false,'true','false');
+
+let email="deneme@hotmail.com";
+let adSoyad=['can','tuce','elif']
+
+
+let varmi = email.includes('g');
+//console.log(varmi);
+
+let varmi2 =adSoyad.includes('tuce')
+//console.log(varmi2);
+
+let yas=29;
+// console.log(yas == 29);
+// console.log(yas == 35);
+// console.log(yas != 35);
+// console.log(yas > 25);
+// console.log(yas < 25);
+// console.log(yas >= 29);
+// console.log(yas <= 29);
+
+let ad ='can';
+console.log(ad == 'can');
+console.log(ad == 'Can');
+console.log(ad > 'Can');
+console.log(ad > 'adnan');
+
+
