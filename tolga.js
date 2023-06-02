ab8f7ea2d12b749f3bf23ce58773072ad6b21397
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 21:03:05 +0300

    for

---
+++ tolga.js
@@ -0,0 +1,13 @@
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+console.log("Döngü Bitti");
+
+const adSoyad=['can','tuce','elif']
+
+for(let i=0;i<adSoyad.length;i++)
+{
+    console.log(adSoyad[i]);
+    let html= `<div>${adSoyad[i]} </div>`
+    console.log(html);
+}
