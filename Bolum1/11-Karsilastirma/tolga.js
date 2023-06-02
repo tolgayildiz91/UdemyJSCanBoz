2bc6aa1367973a304b681ff81426f1c8bf8d76b7
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 20:12:30 +0300

    karşılaştırma

---
+++ Bolum1/11-Karsilastirma/tolga.js
@@ -0,0 +1,27 @@
+let yas="29";
+// console.log(yas+1);
+// console.log(typeof yas);
+
+
+yas = Number(yas);
+console.log(yas+1);
+console.log(typeof yas);
+
+// let sonuc=Number('Can');
+// let sonuc=String(18);
+// let sonuc=Boolean(0);
+// let sonuc=Boolean('');
+// console.log(sonuc,typeof sonuc);
+
+// console.log(yas == 29);
+// console.log(yas == "29");
+
+// console.log(yas != 29);
+// console.log(yas != "29");
+
+
+// console.log(yas === 29);
+// console.log(yas === "29");
+
+console.log(yas !== 29);
+console.log(yas !== "29");
