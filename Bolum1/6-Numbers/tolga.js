602f84119c2285fd2ba5ec5a17374e56d8610083
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 18:52:35 +0300

    numbers

---
+++ Bolum1/6-Numbers/tolga.js
@@ -0,0 +1,32 @@
+let yaricap=15;
+const pisayisi=3.14;
+
+console.log(yaricap,pisayisi);
+
+let dairealani=pisayisi* yaricap**2;
+console.log(dairealani);
+
+console.log(16/4);
+
+let kalan=yaricap %4;
+console.log(kalan);
+
+let sonuc = 6 * (15-4)**2;
+console.log(sonuc);
+
+let sayi=13;
+// sayi=sayi+1;
+// sayi++;
+// sayi +=1;
+
+// sayi *=3;
+// sayi /=3;
+console.log(sayi);
+
+//NaN - Not a Number
+
+console.log(15 / "Can");
+console.log(15 * "Can");
+
+let birlestirme='Dersi '+sayi + ' kere tekrar ettim.';
+console.log(birlestirme);
