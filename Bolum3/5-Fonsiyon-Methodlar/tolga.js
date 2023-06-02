7b11602c28ade183fd3d5083ae65e7bfa9dc0f3b
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 23:32:20 +0300

    Fonksiyon-Method

---
+++ Bolum3/5-Fonsiyon-Methodlar/tolga.js
@@ -0,0 +1,14 @@
+const ad ="tolga";
+
+
+//arrow function
+
+const tolga = () => 'Tolga'
+
+let sonuc1 =tolga();
+console.log(sonuc1);
+
+//method
+
+let sonuc2 = ad.toUpperCase();
+console.log(sonuc2);
