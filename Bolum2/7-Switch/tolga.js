3c1e9f89232913edc410045f5c0f793eb305cc6a
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 21:59:18 +0300

    switch-case

---
+++ Bolum2/7-Switch/tolga.js
@@ -0,0 +1,39 @@
+const not = '20';
+
+switch(not)
+{
+        case 20:
+        console.log('En başarılı sensin');
+        break;
+        case 'BA':
+        console.log('Süper not');
+        break;
+        case 'BB':
+        console.log('Güzel not');
+        break;
+        case 'CB':
+        console.log('İdare eder');
+        break;
+        case 'CC':
+        console.log('Hadi yine iyiysin geçtin dersi');
+        break;
+        default:
+        console.log('Hobaa');
+        break;
+
+}
+
+
+// if(not === 'AA'){
+
+// } else if(not === 'BA'){
+
+// } else if(not === 'BB'){
+
+// } else if(not === 'CB'){
+
+// } else if(not === 'CC'){
+
+// } else {
+
+// }
