74c825bfa1d1b6535b14cfd0bc56c1b69fd37ffc
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 23:07:19 +0300

    return

---
+++ Bolum3/3-Return/tolga.js
@@ -0,0 +1,13 @@
+
+const karealani =function(kenar){
+    // let alan=kenar**2;
+    // // console.log(alan);
+    // return alan;
+
+    return kenar **2;
+}
+
+
+const sonuc= karealani(6);
+
+console.log(sonuc);
