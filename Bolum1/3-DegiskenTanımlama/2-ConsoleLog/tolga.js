125d5cd58021536569a42dbd5df325f0f8f1701e
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 18:28:00 +0300

    değişken tanımlama

---
+++ Bolum1/3-DegiskenTanımlama/2-ConsoleLog/tolga.js
@@ -0,0 +1,19 @@
+let gun=16;
+let ay=2;
+let yil=1991;
+console.log(gun,ay,yil);
+
+console.log("Tolga "+gun+"/"+ay+"/"+yil+" tarihinde doğmuştur");
+
+gun=29;
+console.log(gun);
+
+const sifirarabayafiyati=250000;
+//sifirarabayafiyati=100000;
+console.log(sifirarabayafiyati);
+
+
+/*
+var arabafiyati=130000;
+console.log(arabafiyati);
+*/
