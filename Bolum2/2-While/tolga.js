4caff282fdcefa062bb19fba4152e146e44ddfad
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 21:10:13 +0300

    while

---
+++ Bolum2/2-While/tolga.js
@@ -0,0 +1,23 @@
+const adSoyad = ['can', 'tuce', 'elif'];
+let i=0;
+
+while(i < 10)
+{
+    console.log(i);
+    i++;
+}
+
+i=0;
+
+while(i < adSoyad.length)
+{
+    console.log(adSoyad[i]);
+    i++;
+}
+
+i=6;
+while(i>3)
+{
+    console.log(i);
+    i--;
+}
