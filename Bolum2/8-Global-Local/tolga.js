0ae2b92544c2b9b126b0b44092dc33b4790914a9
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-02 22:04:33 +0300

    global-local

---
+++ Bolum2/8-Global-Local/tolga.js
@@ -0,0 +1,18 @@
+let yas = 29;
+
+
+
+if(true)
+{
+    let yas =60;   
+    console.log("İçerideki:",yas);
+    if(true)
+    {
+        let yas =50;
+        let ad="Can";
+        let cantest="Hobaa";
+        console.log("İçeridekinin içindeki:",yas,ad,cantest);
+    }
+}
+
+console.log("Dışarıdaki:",yas,cantest);
