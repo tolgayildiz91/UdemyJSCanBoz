7d5f0fc06c41c61a2f7e94065e81f7430bedf131
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-03 21:36:18 +0300

    foreach

---
+++ Bolum3/6-Foreach/tolga.js
@@ -0,0 +1,28 @@
+// const a = (callBacka) =>{
+//     let yas=40;
+//     callBacka(yas);
+//   };
+//   a(function(value)
+//   {
+//     console.log(value);
+//   })
+  
+
+let ogrencilerim=['can','hakan','elif','tuba'];
+
+ogrencilerim.forEach(function()
+{
+    console.log("Selam");
+})
+
+ogrencilerim.forEach(function(kisi,index)
+{
+    console.log(kisi,index);
+})
+
+const ogrenci = (kisi,index) =>
+{
+    console.log(`${index} - ${kisi}`);
+}
+
+ogrencilerim.forEach(ogrenci);
