4bea0b95604db2832d9fbae8be960b1abfda1db6
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-03 22:40:30 +0300

    Math Object

---
+++ Bolum4/5-Math-Object/tolga.js
@@ -0,0 +1,16 @@
+console.log(Math);
+console.log(Math.PI);
+
+
+let sayi=5.3;
+
+console.log(Math.round(sayi));//En yakın
+console.log(Math.floor(sayi));//1 alta yuvarla
+console.log(Math.ceil(sayi));//1 üste yuvarla
+console.log(Math.trunc(sayi));//decimal kısmı uçur
+
+
+const rastgele=Math.random();
+
+console.log(rastgele);
+console.log(Math.round(rastgele*100));
