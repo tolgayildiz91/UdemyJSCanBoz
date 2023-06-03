36a279dea507d45f95e7c6d56d4392dd2709898b
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-03 21:43:40 +0300

    foreach örnek

---
+++ Bolum3/7-ForeachOrnek/tolga.js
@@ -0,0 +1,14 @@
+const ulum=document.querySelector('.main');
+
+let ogrencilerim=['can','hakan','elif','tuba'];
+
+let html=``;
+
+ogrencilerim.forEach(ogrenci =>{
+    html += `<li>${ogrenci}</li>`
+})
+
+console.log(html);
+
+
+ulum.innerHTML+=html;
