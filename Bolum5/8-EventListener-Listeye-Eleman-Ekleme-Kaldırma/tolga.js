faa806be311dcb15d801f08c1c2e6cb4a2782982
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-05 02:23:48 +0300

    EventListener-Listeye-Eleman-Ekleme-Kaldırma

---
+++ Bolum5/8-EventListener-Listeye-Eleman-Ekleme-Kaldırma/tolga.js
@@ -0,0 +1,39 @@
+const button=document.querySelector('button');
+
+// button.addEventListener('click',() =>{
+//     console.log('Tıklandı');
+// })
+
+const liElemanlari=document.querySelectorAll('li');
+
+liElemanlari.forEach(eleman => {
+
+    eleman.addEventListener('click',e=>{
+         console.log(e);
+         console.log(e.target);
+         console.log('Liye tıklandı');
+         e.target.style.color='blue';
+    })
+})
+
+
+const ul=document.querySelector('ul');
+//ul.remove();
+
+
+liElemanlari.forEach(eleman => {
+
+    eleman.addEventListener('click',e=>{
+        e.target.remove();
+    })
+})
+
+
+button.addEventListener('click',() =>{
+   const li=document.createElement('li');
+   li.textContent='Javascript';
+
+//    ul.append(li);
+   ul.prepend(li);
+
+})
