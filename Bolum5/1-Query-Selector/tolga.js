9b2553e3a3d4ccf481622dd3c6f189bf37c2b6a9
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-05 00:26:45 +0300

    query selector

---
+++ Bolum5/1-Query-Selector/tolga.js
@@ -0,0 +1,12 @@
+// const hata=document.querySelector('p');
+// const hata=document.querySelector('.error');
+// const hata=document.querySelector('div.error');
+// console.log(hata);
+
+const hatalar=document.querySelectorAll('p');
+console.log(hatalar);
+console.log(hatalar[1]);
+
+hatalar.forEach(hata =>{
+    console.log(hata);
+})
