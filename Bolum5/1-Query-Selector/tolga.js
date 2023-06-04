cf99e6061f6ebb7543698c7679d32beef3e25c2f
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-05 00:46:57 +0300

    GetElementByID

--- Bolum5/1-Query-Selector/tolga.js
+++ Bolum5/1-Query-Selector/tolga.js
@@ -1,11 +1,12 @@
-// const hata=document.querySelector('p');
+//const hata=document.querySelector('p');
 // const hata=document.querySelector('.error');
-// const hata=document.querySelector('div.error');
-// console.log(hata);
+ const hata=document.querySelector('div.error');
 
-const hatalar=document.querySelectorAll('p');
-console.log(hatalar);
-console.log(hatalar[1]);
+console.log(hata);
+
+ const hatalar=document.querySelectorAll('p');
+ console.log(hatalar);
+ console.log(hatalar[0]);
 
 hatalar.forEach(hata =>{
     console.log(hata);
---
+++ Bolum5/2-GetElementByID-GetElementsByClassName-GetElementsByTagName/tolga.js
@@ -0,0 +1,13 @@
+const baslik = document.getElementById('Tolga')
+console.log(baslik)
+
+const hatalar = document.getElementsByClassName('error')
+console.log(hatalar[0])
+
+// hatalar.forEach(hata => {
+//     console.log(hata)
+// });
+
+
+const pEtikleri = document.getElementsByTagName('p')
+console.log(pEtikleri)
