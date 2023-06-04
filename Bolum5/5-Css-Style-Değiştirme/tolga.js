af0c79b32e7d2b00879c2f2706b713e0f7e6882f
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-05 01:18:08 +0300

    Css-Style-Değiştirme

---
+++ Bolum5/5-Css-Style-Değiştirme/tolga.js
@@ -0,0 +1,7 @@
+const baslik = document.querySelector('h1');
+//baslik.setAttribute('style','padding:50px;')
+console.log(baslik.style);
+
+baslik.style.padding='50px';
+baslik.style.fontSize='70px';
+baslik.style.fontWeight='bold';
