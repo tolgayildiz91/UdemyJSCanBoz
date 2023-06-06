e5ca86123d1e2bb4a8835ce50fc7b2eab7f9012a
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-06 14:28:08 +0300

    Timestamp-İkiTarihArasındakiFarkiBulma

---
+++ Bolum10/2-Timestamp-İkiTarihArasındakiFarkiBulma/tolga.js
@@ -0,0 +1,19 @@
+const startDate=new Date('08/15/2018 9:45:00');
+const now=new Date();
+console.log(startDate);
+
+const diff=now.getTime()-startDate.getTime();
+console.log(diff);
+
+const mins=Math.round(diff/1000/60);
+const hours=Math.round(mins/60);
+const days=Math.round(hours/24);
+const years=Math.round(days/365);
+console.log(`Video çekmeye ${mins} dakika önce başladım`);
+console.log(`Video çekmeye ${hours} saat önce başladım`);
+console.log(`Video çekmeye ${days} gün önce başladım`);
+console.log(`Video çekmeye ${years} yıl önce başladım`);
+
+
+const timestamp=1627727162629;
+console.log(new Date(timestamp));
