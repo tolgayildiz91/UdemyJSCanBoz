9b0e94448a4bfd858bb02e12e7f086a6ccbae8b0
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-06 17:43:24 +0300

    Json

---
+++ Bolum12/5-Json/can.js
@@ -0,0 +1,23 @@
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        callback(undefined, data);
+      } else if (request.readyState === 4) {
+        callback('Başarılı cevap alamadık!', undefined);
+      }
+    });
+  
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.send();
+  };
+  
+  getTodos((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+  
