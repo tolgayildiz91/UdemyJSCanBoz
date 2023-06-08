0d6ef8aa8b58191e58421ea88a8bc2dc3b040a86
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-08 19:03:19 +0300

    SubClasses

---
+++ Bolum14/4-SubClasses/can.js
@@ -0,0 +1,32 @@
+class User {
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  login() {
+    console.log(`${this.username} giriş yaptı`);
+    return this;
+  }
+
+  logout() {
+    console.log(`${this.username} çıkış yaptı`);
+    return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(userArrived) {
+    users = users.filter((user) => user.username !== userArrived.username);
+  }
+}
+
+const userOne = new User('can', 'ccc@hotmail.com');
+const userTwo = new User('nida', 'nnn@hotmail.com');
+const userThree = new Admin('osman', 'osman@hotmail.com');
+
+let users = [userOne, userTwo, userThree];
+userThree.deleteUser(userOne);
+
+console.log(userOne, userTwo, userThree);
+console.log(users);
