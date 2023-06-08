b1a9299b138ea0ab1afabd4d02f3d387fa3c3764
Tolga YILDIZ <tolgayildiz91@hotmail.com.tr>
2023-06-08 18:00:01 +0300

    GetSetData

---
+++ Bolum13/1-GetSetData/can.js
@@ -0,0 +1,15 @@
+localStorage.setItem('name', 'Can');
+localStorage.setItem('age', 29);
+
+let ad = localStorage.getItem('name');
+let yas = localStorage.getItem('age');
+
+console.log(ad, yas);
+
+localStorage.setItem('name', 'Can Boz');
+localStorage.age = 30;
+
+ad = localStorage.getItem('name');
+yas = localStorage.getItem('age');
+
+console.log(ad, yas);
